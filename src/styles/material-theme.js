610f626a4dabb9e99726c2a8eb6d7641afc553bc
lapc18 <lapc18@outlook.com>
2022-03-23 19:11:16 -0400

    feat: add custom mui theme

---
+++ src/styles/material-theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+
+const customTheme = createTheme({
+    palette: {
+        primary: {
+          main: '#ff8a80',
+        },
+        secondary: {
+          main: '#e53935',
+        },
+    },
+});
+
+
+export default customTheme;
