31975c76a3bd8bf651528cf012bddff2f1dde73e
lapc18 <lapc18@outlook.com>
2022-03-25 14:18:25 -0400

    feat: add slice functions

--- src/store/slices/noteSlice.js
+++ src/store/slices/noteSlice.js
@@ -1,12 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState = [];
+const initialState = {
+  currentSelected: {},
+  list: [],
+};
 
 const noteSlice = createSlice({
-    name: 'notes',
-    initialState,
-    reducers: {}
+  name: "notes",
+  initialState,
+  reducers: {
+    noteAdded(state, action) {
+      state.list.push(action.payload);
+    },
+    noteRemove(state, action) {
+      state.list = [...state.list.filter((x) => x.id !== action.payload)];
+      return state;
+    },
+    noteEdit(state, action) {
+      state.currentSelected = action.payload;
+    },
+    noteSaveEdit(state, action) {
+      state.list = [...state.list.map(x => {
+        if(x.id === action.payload.id) return { ...action.payload };
+        return x;
+      })];
+    }
+  },
 });
 
+export const { noteAdded, noteRemove, noteEdit, noteSaveEdit } = noteSlice.actions;
 export default noteSlice.reducer;
